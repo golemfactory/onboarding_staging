c8777267c18e2c68e2a3b1234fbfafcb9f34f851
EvilSeeQu-sys <85349934+EvilSeeQu-sys@users.noreply.github.com>
2025-06-11 17:09:24 +0200

    Update Footer.tsx

--- frontend/src/components/templates/themes/defaultTheme/Footer.tsx
+++ frontend/src/components/templates/themes/defaultTheme/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="inline h-4 mb-8 col-span-12 flex flex-col sm:flex-row justify-between text-c-n whitespace-nowrap">
-      <div>Copyright © 2024 Golem Factory GmbH</div>
+      <div>Copyright © 2025 Golem Factory GmbH</div>
       <div className="inline flex gap-1 text-kanit">
         <div>Made with</div>
         <img src="heart.svg" />
